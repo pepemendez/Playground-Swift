946e83b3753b4f5c50e172e74a8fa4d349edb78b
Jose <jose@mail.com>
2019-06-30 00:31:07 -0500

    We don't need to set the white squares since our background is white

--- n-reinas/MyPlayground.playground/Sources/UIBoard.swift
+++ n-reinas/MyPlayground.playground/Sources/UIBoard.swift
@@ -4,6 +4,7 @@ public protocol SolverDelegate: class{
     func running()
     func improvement(f: Int)
     func done()
+    func time(seconds: Double)
 }
 
 public class UIBoard: UIView, SolutionDelegate {
@@ -49,27 +50,21 @@ public class UIBoard: UIView, SolutionDelegate {
         tablero2.Solve()
     }
     
-    
     /// We draw our squares and we check if for some square there is a queen, if it's then we draw a circle
     func drawChess(currGraphicsContext: CGContext?){
-        
+        let start = Date()
+
         var whites = [CGRect]()
         var black = [CGRect]()
         var reds = [CGRect]()
 
         for i in 0 ..< size {
             for j in 0 ..< size {
-                if( (i + j) % 2 == 0 ){
-                    whites.append(CGRect(x: (self.frame.width/CGFloat(size))*CGFloat(j),
+                if( (i + j) % 2 == 1 ){
+                    black.append(CGRect(x: (self.frame.width/CGFloat(size))*CGFloat(j),
                                          y: (self.frame.width/CGFloat(size))*CGFloat(i),
                                          width: self.frame.width/CGFloat(size),
                                          height: self.frame.width/CGFloat(size)))
-                }
-                else{
-                    black.append(CGRect(x: (self.frame.width/CGFloat(size))*CGFloat(j),
-                                        y: (self.frame.width/CGFloat(size))*CGFloat(i),
-                                        width: self.frame.width/CGFloat(size),
-                                        height: self.frame.width/CGFloat(size)))
                 }
                                 
                 if(tablero2.getTablero()[i][j] > 0){
@@ -90,6 +85,10 @@ public class UIBoard: UIView, SolutionDelegate {
         currGraphicsContext?.setFillColor(UIColor.red.cgColor)
         currGraphicsContext?.fill(reds)
 
+        let now = Date()
+        let time =  now.timeIntervalSince(start)
+        
+        delegate?.time(seconds: time)
     }
     
     /// We draw the outer borders for our chess board
